d1ba89a4532f59b54a9e51156d0d3b36385758f2
OscarPOstos <110192091+OscarPOstos@users.noreply.github.com>
2022-12-21 09:51:53 +0100

    Update streamlit.app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,27 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-"""
-# Welcome to Streamlit!
+import streamlit as st
+import joblib
+import pandas as pd
+from PIL import Image
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+image = Image.open('sus.gif')
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+st.title("Streamlit Machine Learning App")
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# Add a text input
+st.image(image, caption='among us')
+height = st.number_input("Enter your Height:", step=1.0)
+weight = st.number_input("Enter your Weight:", step=1.0)
+color_eyes = st.selectbox("Enter your color eyes:", ('Brown', 'Blue'))
+submit = st.button("Submit")
 
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+# Display the entered name
+if submit:
+    pickled_model = joblib.load(open('pet_model.pkl', 'rb'))
+    X = pd.DataFrame([[height, weight, color_eyes]], columns=["Height", "Weight", "Eye"])
+    X = X.replace(["Brown", "Blue"], [1, 0])
+    prediction = pickled_model.predict(X)[0]
+    st.text(prediction)
